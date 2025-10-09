3a3e729392828e8c0c201a87e4f226be67f323ac
Jeremy12go <Jeremy12go@gmail.com>
2025-10-09 15:41:59 -0300

    ajuste del panel de register

--- frontend/my-app/src/pages/Register.js
+++ frontend/my-app/src/pages/Register.js
@@ -1,3 +1,4 @@
+import '../styles/register.css'
 import React, { useState } from 'react';
 import { registerAccount } from '../API/APIGateway';
 
@@ -27,71 +28,53 @@ function Register({ cambiarPantalla }) {
 
     return (
         <div class="App">
-            <div style={{ textAlign: 'center' }}>
-                <p style={{ fontSize: '35px', marginBottom: '4px', marginTop: '0px'}}
-                    className="text-titulos">
+            <div>
+                <p className="text-titulos">
                     Registro
                 </p>
-                <div style={{ fontSize: '16px', textAlign: 'left', maxWidth: '300px', margin: '0 auto', marginTop: '30px'}}>
+                <div>
                     
-                    <p style={{marginBottom: '10px'}}
-                        className="text-common">
+                    <p className="text-common">
                         Nombre de usuario* </p>
                     <input type="name" placeholder="Usuario" 
                         value={name} onChange={ (e) => setName(e.target.value) }
                         className="input-text" />   
 
-                    <p style={{marginBottom: '10px'}}
-                        className="text-common">
+                    <p className="text-common">
                         Telefono*</p>
                     <input type="phoneNumber" placeholder="911111111"
                         value={phoneNumber} onChange={ (e) => setPhoneNumber(e.target.value) }
                         className="input-text" />
 
-                    <p style={{marginBottom: '10px'}}
-                        className="text-common">
+                    <p className="text-common">
                         Dirección*</p>
                     <input type="address" placeholder="Ciudad/Calle/Numero"
                         value={address} onChange={ (e) => setAddress(e.target.value) }
                         className="input-text" />
 
-                    <p style={{marginBottom: '10px'}}
-                        className="text-common">
+                    <p className="text-common">
                         Email*</p>
                     <input type="email" placeholder="Email" 
                         value={email} onChange={ (e) => setEmail(e.target.value) }
                         className="input-text" />
 
-                    <p style={{marginBottom: '10px'}}
-                        className="text-common">
+                    <p className="text-common">
                         Contraseña*</p>
                     <input type="password" placeholder="Contraseña"
                         value={password} onChange={ (e) => setPassword(e.target.value) }
                         className="input-text" />
 
-                    <p style={{marginBottom: '10px'}}
-                        className="text-common">
+                    <p className="text-common">
                         Se creara un perfil automaticamente asociado a tu dirección de correo.
                     </p>
                 </div>
             </div>
-            <div style={{marginTop: '20px', marginBottom: '20px'}}>
-                <button
-                    onClick={ handleSubmit }
-                    className="boton-iniciar">
+            <div>
+                <button onClick={ handleSubmit } className="boton-register">
                     Registrar
                 </button>
                 {errorRegistro && (
-                    <p style={{
-                        color: 'red',
-                        textAlign: 'center',
-                        position: 'absolute',
-                        left: '50%',           // Mueve el inicio del elemento al 50% del contenedor
-                        transform: 'translateX(-50%)',  // Retrocede el 50% del ancho del propio elemento
-                        marginTop: '10px',
-                        width: '100%',         // Asegura que el texto no se corte
-                        visibility: errorRegistro ? 'visible' : 'hidden'
-                    }}>
+                    <p style={{ visibility: errorRegistro ? 'visible' : 'hidden' }}>
                         {errorRegistro || ''}
                     </p>
                 )}
