b5991e842eb66ac3c508e573c55fd3fb3e51db67
Francomanx <francomanx6@gmail.com>
2025-11-06 10:40:37 -0300

    Cambios realizados para testeo de DB

--- Back End/service-accounts/models/ProfileBuyer.js
+++ Back End/service-accounts/models/ProfileBuyer.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const profileBuyerSchema = new mongoose.Schema({ 
   name: { type: String, required: true},
   phoneNumber: { type: String, required: true },
+  address: { type: String, required: true },
   orders: [ { type: String, ref: 'Order'} ],
   cart: { type: String },
   books: [ { type: String, ref: 'Book' } ]
